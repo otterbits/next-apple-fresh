53fb2119822cdb08d2d5bd414b0924306668cf85
uoohoon <hoonixox@naver.com>
2024-02-14 15:59:55 +0900

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>
-      <h4 className="title">애플후레시</h4>
-      <p className="sub-title">by otter dev</p>
-    </div>
+    <main>
+      <div className="navbar">
+        <Link href="/">Home</Link>
+        <Link href="/list">List</Link>
+      </div>
+      <div>
+        <h4 className="title">애플후레시</h4>
+        <p className="sub-title">by otter dev</p>
+      </div>
+    </main>
   );
 }
