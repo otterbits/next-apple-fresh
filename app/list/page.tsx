dfbb9b15a26c568329ed612b6f2a2a98bfd9f6ad
uoohoon <hoonixox@naver.com>
2024-02-14 16:59:29 +0900

    feat: add fresh goods

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,13 +1,15 @@
 export default function List() {
+  const goods = ['Tomatoes', 'Pasta', 'Coconut'];
   return (
     <div>
       <h2>Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      {goods.map(function (good) {
+        return (
+          <div className="food">
+            <h4>{good} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
