b902ace9614484c39b5bb9a729cc0efc1a19b04f
uoohoon <hoonixox@naver.com>
2024-02-14 16:04:18 +0900

    refactor: move navbar from page to layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <div className="navbar">
-        <Link href="/">Home</Link>
-        <Link href="/list">List</Link>
-      </div>
       <div>
         <h4 className="title">애플후레시</h4>
         <p className="sub-title">by otter dev</p>
