534c4be9bd0f0d22cc0fe5fef68f8aed50cdaba3
uoohoon <hoonixox@naver.com>
2024-02-14 15:55:45 +0900

    refactor: remove tailwind.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-slate-700">
-      <h4 className="text">애플후레시</h4>
+    <div>
+      <h4 className="title">애플후레시</h4>
       <p className="sub-title">by otter dev</p>
     </div>
   );
