a835e791bdc46efb66f2c32d10f0e2eb8959cd0d
uoohoon <hoonixox@naver.com>
2024-02-15 15:05:24 +0900

    feat: show foods photo

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,9 +3,10 @@ export default function List() {
   return (
     <div>
       <h2>Products</h2>
-      {goods.map(function (good) {
+      {goods.map(function (good, i) {
         return (
-          <div className="food">
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} className="food-img"></img>
             <h4>{good} $40</h4>
           </div>
         );
